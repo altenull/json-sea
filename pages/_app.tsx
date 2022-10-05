6a5322a08878a729c1140e693b00f0f71bbebeba
Heonyoung Kim <altenull@gmail.com>
2022-10-05 22:26:04 +0900

    chore: Wrap application with NextUIProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { NextUIProvider } from "@nextui-org/react";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
