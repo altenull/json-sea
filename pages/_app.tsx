c3d53925273b6c1320f7ea8b7e1de6773c16e42e
Heonyoung Kim <altenull@gmail.com>
2022-10-08 00:05:35 +0900

    buidl: Install recoil module, Wrap application with RecoilRoot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import { NextUIProvider } from "@nextui-org/react";
-import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import { NextUIProvider } from '@nextui-org/react';
+import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <RecoilRoot>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </RecoilRoot>
   );
 }
 
