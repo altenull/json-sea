e5a442532d35cf2cc426a4e912e17753e0331fbf
Heonyoung Kim <altenull@gmail.com>
2022-12-30 17:00:54 +0900

    chore: Apply 'inter' font to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
+import { Inter } from '@next/font/google';
 import Providers from './providers';
 
 type Props = {
   children: React.ReactNode;
 };
 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'] });
+
 const RootLayout = ({ children }: Props) => {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
