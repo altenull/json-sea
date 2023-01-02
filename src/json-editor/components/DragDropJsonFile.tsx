4895f0725e430a8b6cf8281937e7cc0fe2792a5d
Heonyoung Kim <altenull@gmail.com>
2023-01-02 18:24:38 +0900

    feat: Styling `DragDropJsonFile` component, Add `useDragDropJsonFile` hook

--- src/json-editor/components/DragDropJsonFile.tsx
+++ src/json-editor/components/DragDropJsonFile.tsx
@@ -1,124 +1,36 @@
-import { styled, Text } from '@nextui-org/react';
-import { memo, useCallback, useEffect, useId, useRef } from 'react';
-import { useSetRecoilState } from 'recoil';
-import { latestValidStringifiedJsonAtom, stringifiedJsonAtom } from '../../store/json-engine/json-engine.atom';
-import { isValidJson } from '../../utils/json.util';
-import { useBoolean } from '../../utils/react-hooks/useBoolean';
+import { styled, Text, useTheme } from '@nextui-org/react';
+import { memo, useId } from 'react';
+import { Icon } from '../../ui/icon/Icon';
+import { useDragDropJsonFile } from '../hooks/useDragDropJsonFile';
 
 type Props = {
-  successCallback: () => void;
+  afterFileReadSuccess: () => void;
 };
 
-// TODO: Styling
-const _DragDropJsonFile = ({ successCallback }: Props) => {
-  const setStringifiedJson = useSetRecoilState(stringifiedJsonAtom);
-  const setLatestValidStringifiedJson = useSetRecoilState(latestValidStringifiedJsonAtom);
-
-  const { bool: isDragging, setTrue: onIsDragging, setFalse: offIsDragging } = useBoolean();
-  const dragRef = useRef<HTMLLabelElement | null>(null);
-  const fileInputId = useId();
-
-  const updateJsonStore = useCallback(
-    (jsonFileText: string) => {
-      if (isValidJson(jsonFileText)) {
-        const stringifiedJson: string = JSON.stringify(JSON.parse(jsonFileText), null, 2);
-
-        setStringifiedJson(stringifiedJson);
-        setLatestValidStringifiedJson(stringifiedJson);
-        successCallback();
-      }
-    },
-    [setStringifiedJson, setLatestValidStringifiedJson, successCallback]
-  );
-
-  const handleDragIn = useCallback(
-    (e: DragEvent): void => {
-      e.preventDefault();
-      e.stopPropagation();
-
-      onIsDragging();
-    },
-    [onIsDragging]
-  );
-
-  const handleDragOut = useCallback(
-    (e: DragEvent): void => {
-      e.preventDefault();
-      e.stopPropagation();
-
-      offIsDragging();
-    },
-    [offIsDragging]
-  );
-
-  const handleDragOver = useCallback(
-    (e: DragEvent): void => {
-      e.preventDefault();
-      e.stopPropagation();
-
-      onIsDragging();
-    },
-    [onIsDragging]
-  );
-
-  const handleDrop = useCallback(
-    (e: DragEvent): void => {
-      e.preventDefault();
-      e.stopPropagation();
-
-      offIsDragging();
-
-      if (e.dataTransfer?.files && e.dataTransfer?.files[0]) {
-        const file: File = e.dataTransfer.files[0];
-
-        if (file.type === 'application/json') {
-          file.text().then((jsonText: string) => {
-            updateJsonStore(jsonText);
-          });
-        }
-      }
-    },
-    [offIsDragging, updateJsonStore]
-  );
-
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> = useCallback(
-    (e) => {
-      e.preventDefault();
-
-      if (e.target.files && e.target.files[0]) {
-        const jsonFile: File = e.target.files[0]; // <input> tag has accept="application/JSON" attribute.
-        jsonFile.text().then((jsonText: string) => {
-          updateJsonStore(jsonText);
-        });
-      }
-    },
-    [updateJsonStore]
-  );
-
-  useEffect(() => {
-    if (!!dragRef?.current) {
-      dragRef.current.addEventListener('dragenter', handleDragIn);
-      dragRef.current.addEventListener('dragleave', handleDragOut);
-      dragRef.current.addEventListener('dragover', handleDragOver);
-      dragRef.current.addEventListener('drop', handleDrop);
-    }
-
-    return () => {
-      if (!!dragRef?.current) {
-        dragRef.current.removeEventListener('dragenter', handleDragIn);
-        dragRef.current.removeEventListener('dragleave', handleDragOut);
-        dragRef.current.removeEventListener('dragover', handleDragOver);
-        dragRef.current.removeEventListener('drop', handleDrop);
-      }
-    };
-  }, [dragRef, handleDragIn, handleDragOut, handleDragOver, handleDrop]);
+const _DragDropJsonFile = ({ afterFileReadSuccess }: Props) => {
+  const fileInputId: string = useId();
+  const { theme } = useTheme();
+  const { dropzoneRef, isDragging, handleFileInputChange } = useDragDropJsonFile(afterFileReadSuccess);
 
   return (
     <StyledHost>
-      <StyledFileInput type="file" accept="application/JSON" id={fileInputId} onChange={handleChange} />
+      <StyledFileInput type="file" accept="application/JSON" id={fileInputId} onChange={handleFileInputChange} />
 
-      <StyledLabel ref={dragRef} htmlFor={fileInputId}>
-        <Text>{isDragging ? '(Dragging!)' : ''}Drop a your JSON file, or click here</Text>
+      <StyledLabel
+        ref={dropzoneRef}
+        css={{
+          backgroundColor: isDragging ? '$successLight' : '$gray50',
+        }}
+        htmlFor={fileInputId}
+      >
+        <Icon
+          icon="cloud-upload"
+          size={40}
+          color={isDragging ? theme?.colors.success.value : theme?.colors.accents8.value}
+        />
+        <Text size="$sm" color={isDragging ? 'success' : 'default'}>
+          {isDragging ? 'Drop it to import' : 'Drop a your JSON file, or click here'}
+        </Text>
       </StyledLabel>
     </StyledHost>
   );
@@ -128,8 +40,7 @@ const StyledHost = styled('div', {
   position: 'relative',
   width: '100%',
   maxWidth: '100%',
-  height: '160px',
-  backgroundColor: '$gray100',
+  height: '200px',
 });
 
 const StyledFileInput = styled('input', {
@@ -140,14 +51,16 @@ const StyledLabel = styled('label', {
   width: '100%',
   height: '100%',
   display: 'flex',
+  flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
   cursor: 'pointer',
-  borderWidth: '2px',
-  borderRadius: '1rem',
-  borderStyle: 'dashed',
-  borderColor: '#cbd5e1',
-  backgroundColor: '#f8fafc',
+  borderRadius: '$lg',
+  border: '2px dashed $border',
+
+  '&:hover': {
+    backgroundColor: '$gray100',
+  },
 });
 
 export const DragDropJsonFile = memo(_DragDropJsonFile);
---
+++ src/json-editor/hooks/useDragDropJsonFile.ts
@@ -0,0 +1,112 @@
+import { useCallback, useEffect, useRef } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { latestValidStringifiedJsonAtom, stringifiedJsonAtom } from '../../store/json-engine/json-engine.atom';
+import { formatJsonLikeData, isValidJson } from '../../utils/json.util';
+import { useBoolean } from '../../utils/react-hooks/useBoolean';
+
+export const useDragDropJsonFile = (afterFileReadSuccess: () => void) => {
+  const setStringifiedJson = useSetRecoilState(stringifiedJsonAtom);
+  const setLatestValidStringifiedJson = useSetRecoilState(latestValidStringifiedJsonAtom);
+
+  const { bool: isDragging, setTrue: onIsDragging, setFalse: offIsDragging } = useBoolean();
+  const dropzoneRef = useRef<HTMLLabelElement | null>(null);
+
+  const processJsonFileText = useCallback(
+    (jsonFileText: string): void => {
+      if (!isValidJson(jsonFileText)) return;
+
+      const formattedJson: string = formatJsonLikeData(jsonFileText);
+
+      setStringifiedJson(formattedJson);
+      setLatestValidStringifiedJson(formattedJson);
+      afterFileReadSuccess();
+    },
+    [setStringifiedJson, setLatestValidStringifiedJson, afterFileReadSuccess]
+  );
+
+  const handleDragIn = useCallback(
+    (e: DragEvent): void => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      onIsDragging();
+    },
+    [onIsDragging]
+  );
+
+  const handleDragOut = useCallback(
+    (e: DragEvent): void => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      offIsDragging();
+    },
+    [offIsDragging]
+  );
+
+  const handleDragOver = useCallback(
+    (e: DragEvent): void => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      onIsDragging();
+    },
+    [onIsDragging]
+  );
+
+  const handleDrop = useCallback(
+    (e: DragEvent): void => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      offIsDragging();
+
+      if (e.dataTransfer?.files && e.dataTransfer?.files[0]) {
+        const file: File = e.dataTransfer.files[0];
+
+        if (file.type === 'application/json') {
+          file.text().then((jsonText: string) => {
+            processJsonFileText(jsonText);
+          });
+        }
+      }
+    },
+    [offIsDragging, processJsonFileText]
+  );
+
+  const handleFileInputChange: React.ChangeEventHandler<HTMLInputElement> = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      if (e.target.files && e.target.files[0]) {
+        const jsonFile: File = e.target.files[0]; // <input> tag has accept="application/JSON" attribute.
+        jsonFile.text().then((jsonText: string) => {
+          processJsonFileText(jsonText);
+        });
+      }
+    },
+    [processJsonFileText]
+  );
+
+  useEffect(() => {
+    const dropzoneElement: HTMLLabelElement | null = dropzoneRef?.current;
+
+    dropzoneElement?.addEventListener('dragenter', handleDragIn);
+    dropzoneElement?.addEventListener('dragleave', handleDragOut);
+    dropzoneElement?.addEventListener('dragover', handleDragOver);
+    dropzoneElement?.addEventListener('drop', handleDrop);
+
+    return () => {
+      dropzoneElement?.removeEventListener('dragenter', handleDragIn);
+      dropzoneElement?.removeEventListener('dragleave', handleDragOut);
+      dropzoneElement?.removeEventListener('dragover', handleDragOver);
+      dropzoneElement?.removeEventListener('drop', handleDrop);
+    };
+  }, [dropzoneRef, handleDragIn, handleDragOut, handleDragOver, handleDrop]);
+
+  return {
+    dropzoneRef,
+    isDragging,
+    handleFileInputChange,
+  };
+};
