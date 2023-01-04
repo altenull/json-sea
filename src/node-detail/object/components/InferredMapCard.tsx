869957d8d96eab0cf774941922c55226c3e24291
Heonyoung Kim <altenull@gmail.com>
2023-01-04 16:56:04 +0900

    feat: Create `InferredMapCard` component

---
+++ src/node-detail/object/components/InferredMapCard.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import { Card, Grid, styled } from '@nextui-org/react';
+import L, { LatLngTuple } from 'leaflet';
+import { memo, useCallback, useEffect, useRef } from 'react';
+import { PropertyKeyBadge } from './PropertyKeyBadge';
+
+type Props = {
+  latLngPropertyKeys: [string, string];
+  latLng: LatLngTuple;
+};
+
+const _InferredMapCard = ({ latLngPropertyKeys, latLng }: Props) => {
+  const leafletMapRef = useRef<HTMLDivElement | null>(null);
+
+  const isLeafletInitialized = useCallback((leafletMapElement: HTMLDivElement): boolean => {
+    const mapContainer: HTMLElement | null = L.DomUtil.get(leafletMapElement);
+    return (mapContainer as any)._leaflet_id !== undefined;
+  }, []);
+
+  const addTileLayer = useCallback((leafletMap: L.Map) => {
+    const attribution: string = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution,
+    }).addTo(leafletMap);
+  }, []);
+
+  useEffect(() => {
+    if (!leafletMapRef.current) return;
+
+    const leafletMapElement: HTMLDivElement = leafletMapRef.current;
+
+    if (isLeafletInitialized(leafletMapElement)) {
+      return;
+    }
+
+    const leafletMap: L.Map = L.map(leafletMapElement, {
+      center: latLng,
+      zoom: 12,
+      preferCanvas: true,
+    });
+
+    addTileLayer(leafletMap);
+    L.marker(latLng).addTo(leafletMap);
+
+    return () => {
+      leafletMap.off();
+      leafletMap.remove();
+    };
+  }, [latLng, isLeafletInitialized, addTileLayer]);
+
+  const [latPropertyK, lngPropertyK] = latLngPropertyKeys;
+
+  // TODO: Styling
+  return (
+    <Card>
+      <Card.Header>
+        <Grid.Container direction="column">
+          <Grid>
+            <PropertyKeyBadge propertyK={latPropertyK} /> + <PropertyKeyBadge propertyK={lngPropertyK} />
+          </Grid>
+        </Grid.Container>
+      </Card.Header>
+
+      <Card.Body css={{ paddingTop: '$sm' }}>
+        <StyledMap ref={leafletMapRef} />
+      </Card.Body>
+    </Card>
+  );
+};
+
+const StyledMap = styled('div', {
+  width: '100%',
+  height: '240px',
+});
+
+export const InferredMapCard = memo(_InferredMapCard);
