d4e534bf414f3b5b73954b9afe9a89af3056c034
Heonyoung Kim <altenull@gmail.com>
2023-01-06 16:19:39 +0900

    feat: Add `file-block`, `file-check`, `left-arrow-with-bar` and `right-arrow-with-bar` icons

--- src/ui/icon/icon.type.ts
+++ src/ui/icon/icon.type.ts
@@ -1,5 +1,17 @@
 import { tuple } from '../../utils/function.util';
 
-export const iconNames = tuple('file-plus', 'download', 'cloud-upload', 'sun', 'moon', 'heart', 'github');
+export const iconNames = tuple(
+  'file-plus',
+  'file-block',
+  'file-check',
+  'left-arrow-with-bar',
+  'right-arrow-with-bar',
+  'download',
+  'cloud-upload',
+  'sun',
+  'moon',
+  'heart',
+  'github'
+);
 
 export type IconName = typeof iconNames[number];
