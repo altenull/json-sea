6138ff573684a4e883dd93166cbfbaf327f95510
Heonyoung Kim <altenull@gmail.com>
2023-01-11 22:45:47 +0900

    fix: Set `id` prop of all `Navbar.Item` component to resolve warning

--- src/foundation/components/GlobalNav.tsx
+++ src/foundation/components/GlobalNav.tsx
@@ -1,16 +1,25 @@
 'use client';
 
 import { Navbar } from '@nextui-org/react';
+import { useId } from 'react';
 import { GithubButton } from './GithubButton';
 import { JsonEditorToggle } from './JsonEditorToggle';
 import { JsonSeaLogoTitle } from './JsonSeaLogoTitle';
 import { ThemeToggle } from './ThemeToggle';
 
 const _GlobalNav = () => {
+  /**
+   * Set id prop of each `Navbar.Item` component to resolve below warning message.
+   * @warning - Warning: Prop `id` did not match. Server: "react-aria-1" Client: "react-aria-2"
+   */
+  const navItemId1 = useId();
+  const navItemId2 = useId();
+  const navItemId3 = useId();
+
   return (
     <Navbar isBordered isCompact maxWidth="fluid">
-      <Navbar.Content gap="$4">
-        <Navbar.Item>
+      <Navbar.Content>
+        <Navbar.Item id={navItemId1}>
           <JsonEditorToggle />
         </Navbar.Item>
       </Navbar.Content>
@@ -20,10 +29,11 @@ const _GlobalNav = () => {
       </Navbar.Brand>
 
       <Navbar.Content gap="$4">
-        <Navbar.Item>
+        <Navbar.Item id={navItemId2}>
           <GithubButton />
         </Navbar.Item>
-        <Navbar.Item>
+
+        <Navbar.Item id={navItemId3}>
           <ThemeToggle />
         </Navbar.Item>
       </Navbar.Content>
