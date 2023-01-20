5218d97bce680a1e29248fdf745e84aa54ea5009
Heonyoung Kim <altenull@gmail.com>
2023-01-20 16:52:15 +0900

    fix: Check window object existance

--- src/ui/hooks/useJsonSeaRecommendedWidth.ts
+++ src/ui/hooks/useJsonSeaRecommendedWidth.ts
@@ -4,27 +4,24 @@ import { useEffect, useRef, useState } from 'react';
 import { sizes } from '../constants/sizes.constant';
 
 export const useJsonSeaRecommendedWidth = () => {
+  const isClient: boolean = typeof window === 'object';
   const lastWidth = useRef<number | null>(null);
 
   const [isJsonSeaRecommendedWidth, setIsJsonSeaRecommendedWidth] = useState<boolean>(
-    window?.innerWidth >= sizes.jsonSeaRecommendedWidth
+    isClient ? window.innerWidth >= sizes.jsonSeaRecommendedWidth : false
   );
 
   useEffect(() => {
-    if (typeof window !== 'object') {
-      return undefined;
-    }
-
     const handleResize = () => {
-      if (window.innerWidth !== lastWidth.current) {
+      if (window?.innerWidth !== lastWidth.current) {
         lastWidth.current = window.innerWidth;
 
         setIsJsonSeaRecommendedWidth(window.innerWidth >= sizes.jsonSeaRecommendedWidth);
       }
     };
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window?.addEventListener('resize', handleResize);
+    return () => window?.removeEventListener('resize', handleResize);
   }, []);
 
   return { isJsonSeaRecommendedWidth } as const;
