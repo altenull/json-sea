39b9444896a5e3456f5ac8ca0c8d2f3ecb9df33b
Heonyoung Kim <altenull@gmail.com>
2023-01-28 15:59:14 +0900

    perf: Call `validateStringSubtype` function only once per 'string' node detail card

--- src/node-detail/components/DetailPrimitive.tsx
+++ src/node-detail/components/DetailPrimitive.tsx
@@ -1,31 +1,54 @@
-import { Card, Grid } from '@nextui-org/react';
-import { isValidElement, memo, ReactElement, useMemo } from 'react';
+'use client';
+
+import { Badge, Card, Grid } from '@nextui-org/react';
+import { isValidElement, memo, ReactElement, useEffect, useMemo, useState } from 'react';
 import { JsonDataType } from '../../store/json-engine/enums/json-data-type.enum';
 import { getJsonDataType } from '../../store/json-engine/helpers/json-data-type.helper';
-import { PrimitiveInspector } from '../primitive/components/PrimitiveInspector';
+import { isBoolean, isNull, isNumber, isString } from '../../utils/json.util';
+import { NumberInspector } from '../primitive/components/NumberInspector';
+import { StringInspector } from '../primitive/components/StringInspector';
+import { ALL_FALSE_STRING_SUBTYPE_VALIDATOR } from '../primitive/constants/string-subtype.constant';
+import { StringSubtypeValidator, validateStringSubtype } from '../primitive/helpers/string-subtype.helper';
 import { DataTypeText } from './DataTypeText';
-import { StringSubtypeText } from './StringSubtypeText';
 
 type Props = {
   badge?: ReactElement;
   value: string | number | boolean | null;
 };
 
-const _DetailPrimitive = ({ badge, value }: Props) => {
-  const dataTypeText: ReactElement = useMemo(() => {
-    const jsonDataType: JsonDataType = getJsonDataType(value);
+const getStringSubtypeText = (stringSubtypeValidator: StringSubtypeValidator): string => {
+  if (stringSubtypeValidator.isColor) return '/color';
+  if (stringSubtypeValidator.isDatetime) return '/datetime';
+  if (stringSubtypeValidator.isEmail) return '/email';
+  if (stringSubtypeValidator.isHttpUri) return '/uri';
+  if (stringSubtypeValidator.isImage) return '/image';
+  if (stringSubtypeValidator.isImageUri) return '/imageUri';
+  if (stringSubtypeValidator.isAudio) return '/audio';
+  if (stringSubtypeValidator.isAudioUri) return '/audioUri';
+  if (stringSubtypeValidator.isVideo) return '/video';
+  if (stringSubtypeValidator.isVideoUri) return '/videoUri';
 
-    if (jsonDataType === JsonDataType.String) {
-      return (
-        <>
-          {jsonDataType}
-          <StringSubtypeText value={value as string} />
-        </>
-      );
-    } else {
-      return <>{jsonDataType}</>;
+  return '';
+};
+
+const _DetailPrimitive = ({ badge, value }: Props) => {
+  const [stringSubtypeValidator, setStringSubtypeValidator] = useState<StringSubtypeValidator>(
+    ALL_FALSE_STRING_SUBTYPE_VALIDATOR
+  );
+
+  useEffect(() => {
+    if (isString(value)) {
+      validateStringSubtype(value).then(setStringSubtypeValidator);
     }
   }, [value]);
+
+  const dataTypeText: string = useMemo(() => {
+    const jsonDataType: JsonDataType = getJsonDataType(value);
+
+    return jsonDataType === JsonDataType.String
+      ? jsonDataType.concat(getStringSubtypeText(stringSubtypeValidator))
+      : jsonDataType;
+  }, [value, stringSubtypeValidator]);
 
   return (
     <>
@@ -40,17 +63,15 @@ const _DetailPrimitive = ({ badge, value }: Props) => {
       </Card.Header>
 
       <Card.Body css={{ paddingTop: 0, paddingBottom: '$sm' }}>
-        <PrimitiveInspector value={value} />
+        {isString(value) && <StringInspector stringSubtypeValidator={stringSubtypeValidator} value={value} />}
 
-        {/* <>
-          {isString(value) && <StringInspector value={value} />}
-          {isNumber(value) && <NumberInspector value={value} />}
-          {(isBoolean(value) || isNull(value)) && (
-            <Badge css={{ marginLeft: 'auto' }} isSquared variant="flat">
-              {JSON.stringify(value)}
-            </Badge>
-          )}
-        </> */}
+        {isNumber(value) && <NumberInspector value={value} />}
+
+        {(isBoolean(value) || isNull(value)) && (
+          <Badge css={{ marginLeft: 'auto' }} isSquared variant="flat">
+            {JSON.stringify(value)}
+          </Badge>
+        )}
       </Card.Body>
     </>
   );
--- src/node-detail/primitive/components/StringInspector.tsx
+++ src/node-detail/primitive/components/StringInspector.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react';
 import { encloseDoubleQuote } from '../../../utils/string.util';
-import { useStringSubtypeValidator } from '../hooks/useStringSubtypeValidator';
+import { StringSubtypeValidator } from '../helpers/string-subtype.helper';
 import { HttpUri } from '../types/http-uri.type';
 import { Base64AudioDataUri, Base64ImageDataUri, Base64VideoDataUri } from '../types/media-src.type';
 import { PreviewAudio } from './PreviewAudio';
@@ -15,12 +15,13 @@ import { PreviewVideoUri } from './PreviewVideoUri';
 import { TextCopyBox } from './TextCopyBox';
 
 type Props = {
+  stringSubtypeValidator: StringSubtypeValidator;
   value: string;
 };
 
-const _StringInspector = ({ value }: Props) => {
+const _StringInspector = ({ stringSubtypeValidator, value }: Props) => {
   const { isColor, isDatetime, isEmail, isHttpUri, isImage, isImageUri, isAudio, isAudioUri, isVideo, isVideoUri } =
-    useStringSubtypeValidator(value);
+    stringSubtypeValidator;
 
   return (
     <>
