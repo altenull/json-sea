f93814693f5720b116bb289b3b7d09f33642772d
Heonyoung Kim <altenull@gmail.com>
2023-02-03 22:58:28 +0900

    feat: Apply wave animation to `LandingLoader`

--- src/foundation/components/LandingLoader.tsx
+++ src/foundation/components/LandingLoader.tsx
@@ -1,9 +1,8 @@
 'use client';
 
+import { CSSProperties } from 'react';
 import { useLanding } from '../../store/landing/hooks/useLanding';
 
-// TODO: Styling loading spinner
-// (it would be better having css pure spinner instead of NextUI)
 const _LandingLoader = () => {
   const { isAppInitalized } = useLanding();
 
@@ -11,20 +10,81 @@ const _LandingLoader = () => {
     return null;
   }
 
+  const waveStyle: CSSProperties = {
+    position: 'absolute',
+    left: '50%',
+    minWidth: '300vw',
+    minHeight: '300vw',
+    backgroundColor: '#ffffff',
+    animationName: 'wave-anim',
+    animationIterationCount: 'infinite',
+    animationTimingFunction: 'linear',
+    animationDuration: '10s',
+  };
+
+  /**
+   * Should do styling with pure CSS for better UX.
+   */
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'fixed',
-        inset: 0,
-        zIndex: 9999,
-        backgroundColor: '#ffffff',
-      }}
-    >
-      <h2 style={{ color: 'black', fontSize: '18px' }}>🌊 You are diving into JSON Sea now...</h2>
-    </div>
+    <>
+      <style>
+        {`@keyframes wave-anim {
+            0% {
+              transform: translate(-50%, 0) rotateZ(0deg);
+            }
+            50% {
+              transform: translate(-50%, -2%) rotateZ(180deg);
+            }
+            100% {
+              transform: translate(-50%, 0%) rotateZ(360deg);
+            }
+          }`}
+      </style>
+
+      <div
+        style={{
+          position: 'fixed',
+          inset: '0',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          minHeight: '100vh',
+          backgroundColor: '#96C1F2',
+          overflow: 'hidden',
+          zIndex: 9999,
+        }}
+      >
+        <div
+          data-name="wave1"
+          style={{
+            ...waveStyle,
+            bottom: '15vh',
+            borderRadius: '45%',
+          }}
+        />
+        <div
+          data-name="wave2"
+          style={{
+            ...waveStyle,
+            bottom: '12vh',
+            opacity: '0.5',
+            borderRadius: '47%',
+          }}
+        />
+
+        <div
+          style={{
+            color: '#11181C !important', // text color in light mode
+            fontSize: '18px',
+            fontWeight: 600,
+            zIndex: 10,
+          }}
+        >
+          You are diving into {`'`}JSON Sea{`'`}...
+        </div>
+      </div>
+    </>
   );
 };
 
