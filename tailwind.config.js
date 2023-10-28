914f1a049015f34cadd6af48475bf04de01a2f01
Heonyoung Kim <altenull@gmail.com>
2023-10-28 16:23:14 +0900

    build: install `@nextui-org/tooltip` dependency and configure it

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const { nextui } = require('@nextui-org/theme');
 module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/components/(button|chip|card|navbar).js',
+    './node_modules/@nextui-org/theme/dist/components/(button|chip|card|navbar|tooltip).js',
   ],
   theme: {
     extend: {},
