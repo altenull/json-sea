da23c945dfe4239bd1e62d89fc3c3c5e4c61b63e
Heonyoung Kim <altenull@gmail.com>
2023-10-28 16:37:26 +0900

    chore: install `@nextui-org/modal` dependency and configure it

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const { nextui } = require('@nextui-org/theme');
 module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/components/(button|chip|card|navbar|tooltip|switch).js',
+    './node_modules/@nextui-org/theme/dist/components/(button|chip|card|navbar|tooltip|switch|modal).js',
   ],
   theme: {
     extend: {},
